9d59701152ab1e6422e215cdff97e05810399509
Eze Gatica <ezequielgatica@gmail.com>
2022-05-14 21:08:56 -0300

    Adding bootstrap and react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+}, []);
   return (
     <>
       <Head>
         <title>Ventas, by Eze Gatica</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Link href="/">
         Home
