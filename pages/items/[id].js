17aa202ae679505ebb5ad9990dc4ad3fbf3bccdd
Eze Gatica <ezequielgatica@gmail.com>
2022-05-22 20:39:36 -0300

    Cache

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -6,6 +6,7 @@ import { Alert, Button, Col, Row, Carousel, Container } from 'react-bootstrap';
 import Head from 'next/head';
 import Image from 'next/image';
 import SearchForm from '../../components/search.form';
+import redis from '../../lib/redis';
 
 const ItemPage = ({ item, success }) => {
   const [show, setShow] = useState(item?.vendido || false);
@@ -73,12 +74,19 @@ const ItemPage = ({ item, success }) => {
 export async function getServerSideProps({ params }) {
   await dbConnect();
   if (params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    const item = await Item.findById(params.id).lean();
-    if (!item) {
-      return { props: { item: null, success: false } }
+    const data = await redis.get(`${process.env.NODE_ENV}:item:${params.id}`)
+    if (data) {
+      const result = JSON.parse(data);
+      return { props: { item: result, success: true } };
+    } else {
+      const item = await Item.findById(params.id).lean();
+      if (!item) {
+        return { props: { item: null, success: false } }
+      }
+      item._id = item._id.toString();
+      await redis.set(`${process.env.NODE_ENV}:item:${params.id}`, JSON.stringify(item));
+      return { props: { item: item, success: true } };
     }
-    item._id = item._id.toString();
-    return { props: { item: item, success: true } };
   } else {
     return { props: { item: null, success: false } }
   }
--- pages/items/index.js
+++ pages/items/index.js
@@ -3,6 +3,7 @@ import SearchForm from '../../components/search.form';
 import Item from '../../models/Item';
 import ItemCard from '../../components/item.card';
 import { Row, Container } from 'react-bootstrap';
+import redis from '../../lib/redis';
 
 const Index = ({ items }) => (
   <>
@@ -14,7 +15,7 @@ const Index = ({ items }) => (
             <ItemCard item={item} key={item._id} />
           ))}
         </Row>
-      ) :(
+      ) : (
         <h2>No se encontraron productos con ese término. Prueba con otro</h2>
       )}
     </Container>
@@ -25,31 +26,55 @@ export async function getServerSideProps({ query: queryProp }) {
   await dbConnect();
   const { query } = queryProp;
   let result;
+  let fromCache = false;
   if (query) {
-    result = await Item.find({
-      $or: [
-        {
-          nombre: {
-            $regex: query,
-            $options: 'i'
-          },
-        }, {
-          short_descripcion: {
-            $regex: query,
-            $options: 'i'
+    const data = await redis.get(`${process.env.NODE_ENV}:query:${query}`)
+    if (data) {
+      result = JSON.parse(data);
+      fromCache = true;
+    } else {
+      result = await Item.find({
+        $or: [
+          {
+            nombre: {
+              $regex: query,
+              $options: 'i'
+            },
+          }, {
+            short_descripcion: {
+              $regex: query,
+              $options: 'i'
+            }
           }
-        }
-      ]
-    });
+        ]
+      });
+      await redis.set(`${process.env.NODE_ENV}:query:${query}`, JSON.stringify(result));
+    }
+
   } else {
-    result = await Item.find({});
+    const data = await redis.get(`${process.env.NODE_ENV}:list:all`)
+    if (data) {
+      result = JSON.parse(data);
+      fromCache = true;
+    } else {
+      result = await Item.find({});
+      await redis.set(`${process.env.NODE_ENV}:list:all`, JSON.stringify(result));
+    }
   }
-  const items = result.map(doc => {
-    const item = doc.toObject();
-    item._id = item._id.toString();
-    return item;
-  });
-  return { props: { items: items } };
+  if (fromCache) {
+    return {
+      props: {
+        items: result
+      }
+    }
+  } else {
+    const items = result.map(doc => {
+      const item = doc.toObject();
+      item._id = item._id.toString();
+      return item;
+    });
+    return { props: { items: items || [] } };
+  }
 }
 
 export default Index;
